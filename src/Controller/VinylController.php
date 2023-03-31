173cf519c70ba80b8ae67bc37b7fc14fe66c7b18
AstroAntics <vucinicm@asbindia.org>
2023-03-31 14:57:18 +0800

    Controllers :q !

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class VinylController 
+{
+	#[Route('/')]
+	public function homepage() : Response
+	{
+		return new Response("NOT a frisbee!");
+	}
+}
